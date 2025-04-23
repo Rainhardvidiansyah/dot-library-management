f9b392b260b0a2642b9387c39626480afc9f0f49
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2025-04-23 14:56:11 +0700

    change required roles from enum to string

--- src/common/guards/role.guards.ts
+++ src/common/guards/role.guards.ts
@@ -1,14 +1,20 @@
 
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, Logger } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { UserRole } from '../enums/user-role.enum';
 import { ROLES_KEY } from '../decorators/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
+
+  private logger = new Logger(RolesGuard.name, {timestamp: true});
+
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
+
+    this.logger.log('Roles Guard class is hit');
+
     const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
@@ -17,7 +23,10 @@ export class RolesGuard implements CanActivate {
       return true;
     }
     const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.roles?.includes(role));
+    // return requiredRoles.some((role) => user.roles?.includes(role));
+    
+
+    return requiredRoles.some((role) => user.role.includes(role));
   }
 }
 
